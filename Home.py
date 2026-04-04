3765ae9e06eff1305c4be9bcd4161ca8d7846e9f
Ziyu Liu <2283377027@qq.com>
2026-04-04 21:06:51 +0800

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="MM-HOM Simulation",
+    layout="wide"
+)
+
+st.title("By Lzy @2026-04")
+st.markdown("""
+* **Slow Detector **：快探测器理想化模拟
+* **Fast Detector **：慢探测器理想化模拟
+""")
